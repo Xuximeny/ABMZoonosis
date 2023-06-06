9bc78cace7d31e7cb82bb6c8237c73019ed31122
Meny <diegomeny123@gmail.com>
2023-06-06 12:48:05 -0300

    Cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
+import React from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Nav from './components/Nav'
 import './App.css'
 
 function App() {
@@ -8,28 +10,22 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <Nav></Nav>
+      <h1>Hola Mundo</h1>
+      <h2>Lucas Ojeda || Diego Meny
+      </h2>
+      
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
+
 
 export default App
---
+++ src/Components/Nav.jsx
@@ -0,0 +1,19 @@
+import '../assets/css/Nav.css'
+import  '../assets/tigre.jpg'
+
+function Nav () {
+
+    return (
+        <nav>
+            <div class="logo">
+                </div>
+            <ul>
+            <li>Municipio</li>
+            <li>Contacto</li>
+            <li>Turnos</li>
+        </ul>
+        </nav>
+    )
+}
+
+export default Nav
