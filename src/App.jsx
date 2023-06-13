0e15f85182b89d4e3c4c87ed9bce3b0fdb5768d1
Lucas Ojeda <luquillasojeda@gmail.com>
2023-06-13 09:03:24 -0300

    nc

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import React from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Nav from './components/Nav'
+import Nav from './Components/Nav'
 import './App.css'
 
 function App() {
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,19 +1,28 @@
 import '../assets/css/Nav.css'
-import  '../assets/tigre.jpg'
+import  '../assets/muni.jpg'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faFontAwesome, faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+library.add(faFontAwesome, faBars);
 
 function Nav () {
 
     return (
-        <nav>
+        <nav class="navbar">
+            <div class="logo"></div>
+            <div class="menu-btn"></div>
+            <FontAwesomeIcon icon="bars"/>
             <div class="logo">
-                </div>
-            <ul>
-            <li>Municipio</li>
-            <li>Contacto</li>
-            <li>Turnos</li>
-        </ul>
+                <img src="../assets/css/muni.jpg" alt="logo" />
+                <a href="tigre.jpg">Logo</a> </div>
+             <div class="menu">
+                <a href="Municipio">Municipio</a></div>  
+                <button class="log-in">Cuenta</button> 
+            <div class="menu-btn">
+            <FontAwesomeIcon icon="bars" />
+            </div>
         </nav>
     )
-}
+} 
 
 export default Nav
